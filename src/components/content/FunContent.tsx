cacc13b0d1e9b689d195cf2d61053fa8c7c8b6af
NΞ◎N <90662438+i-am-neon@users.noreply.github.com>
2024-04-28 21:10:41 -0500

    tweak mobile styling and modal padding

--- src/components/content/FunContent.tsx
+++ src/components/content/FunContent.tsx
@@ -1,12 +1,12 @@
 
 export default function FunContent(): JSX.Element {
     return (
-        <div className="flex flex-col sm:mx-16 gap-5">
+        <div className="flex flex-col gap-5">
             <p>
                 <h3 className="text-lg text-primary font-bold mb-2">
                     Some of my interests
                 </h3>
-                <ul className="list-disc">
+                <ul className="list-disc pl-8">
                     <li>long-term travel with just a backpack</li>
                     <i>20+ countries and counting!</i>
                     <li>woodworking</li>
--- src/components/content/SkillsContent.tsx
+++ src/components/content/SkillsContent.tsx
@@ -1,13 +1,14 @@
 export default function SkillsContent() {
     return (
-        <div className="flex flex-col gap-8 pl-4 sm:pl-8">
+        <div className="flex flex-col gap-8 mt-4">
             <div>
                 <h3 className="text-lg text-primary font-bold mb-2">
                     My Bread and Butter
                 </h3>
-                <ul className="list-disc">
+                <ul className="list-disc pl-4">
                     <li>Typescript</li>
-                    <li>React (Next.js, Remix, etc.)</li>
+                    <li>React</li>
+                    <i>Next.js, Remix, etc.</i>
                     <li>Node</li>
                     <li>Python</li>
                 </ul>
@@ -16,7 +17,7 @@ export default function SkillsContent() {
                 <h3 className="text-lg text-primary font-bold mb-2">
                     For Fun
                 </h3>
-                <ul className="list-disc">
+                <ul className="list-disc pl-4">
                     <li>Solidity</li>
                     <li>Stable Diffusion</li>
                     <li>LLMs</li>
