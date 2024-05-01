19e90179e08bcde8a39083b128b8ed7545507626
NΞ◎N <90662438+i-am-neon@users.noreply.github.com>
2024-04-30 21:22:44 -0500

    update meta tags

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -10,13 +10,22 @@ export default function Head() {
         <Helmet>
             <title>{title}</title>
             <meta name="description" content={description} />
-            <meta property="og:title" content={title} />
-            <meta property="og:description" content={description} />
-            <meta property="og:url" content="https://www.neon.lol/" />
-            <meta property="og:image" content={ogImageUrl} />
-            <meta name="twitter:card" content="summary_large_image" />
-            <meta property="og:type" content="website" />
-            <meta name="theme-color" content={currentHexColor} />
+
+            <meta property="og:url" content="https://neon.lol"></meta>
+            <meta property="og:type" content="website"></meta>
+            <meta property="og:title" content={title}></meta>
+            <meta property="og:description" content={description}></meta>
+            <meta property="og:image" content={ogImageUrl}></meta>
+
+            <meta name="twitter:card" content="summary_large_image"></meta>
+            <meta property="twitter:domain" content="neon.lol"></meta>
+            <meta property="twitter:url" content="https://neon.lol"></meta>
+            <meta name="twitter:title" content={title}></meta>
+            <meta name="twitter:description" content={description}></meta>
+            <meta name="twitter:image" content={ogImageUrl}></meta>
+
+            <meta name="theme-color" content={currentHexColor}></meta>
+
             <link rel="icon" href="/favicon.ico" />
         </Helmet>
     )
