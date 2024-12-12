502c64289200f86260edb2eb7402d46b2429a19b
NΞ◎N <90662438+i-am-neon@users.noreply.github.com>
2024-12-12 13:44:58 -0600

    Update FunContent.tsx

--- src/components/content/FunContent.tsx
+++ src/components/content/FunContent.tsx
@@ -1,19 +1,18 @@
-
 export default function FunContent(): JSX.Element {
-    return (
-        <div className="flex flex-col gap-5">
-            <p>
-                <h3 className="text-lg text-primary font-bold mb-2">
-                    Some of my interests
-                </h3>
-                <ul className="list-disc pl-8">
-                    <li>long-term travel with just a backpack</li>
-                    <i>20+ countries and counting!</i>
-                    <li>woodworking</li>
-                    <li>ballroom and latin dance</li>
-                    <li>rock climbing</li>
-                </ul>
-            </p>
-        </div>
-    )
+  return (
+    <div className="flex flex-col gap-5">
+      <p>
+        <h3 className="text-lg text-primary font-bold mb-2">
+          Some of my interests
+        </h3>
+        <ul className="list-disc pl-8">
+          <li>long-term travel with just a backpack</li>
+          <i>25+ countries and counting!</i>
+          <li>woodworking</li>
+          <li>ballroom and latin dance</li>
+          <li>rock climbing</li>
+        </ul>
+      </p>
+    </div>
+  );
 }
